f0a558bc8c5bfbd1ed1f0cd95fc7d0446a4aa5c9
Stuart Wilkins <swilkins@bnl.gov>
2014-11-30 21:27:58 -0500

    UPdated to add 2 TCtrs

--- iocBoot/ioclakeshore336/st.cmd
+++ iocBoot/ioclakeshore336/st.cmd
@@ -23,12 +23,19 @@ drvAsynIPPortConfigure("LAKE2", "10.23.3.11:7777")
 #asynSetTraceIOMask("LAKE2", 0, 0x2)
 
 ## Load record instances
-dbLoadRecords("$(TOP)/db/lakeshore336_chan.db","Sys=XF:23ID1-ES,Dev={TCtrl:1-Chan:A},PORT=LAKE2,CHAN=A")
-dbLoadRecords("$(TOP)/db/lakeshore336_chan.db","Sys=XF:23ID1-ES,Dev={TCtrl:1-Chan:B},PORT=LAKE2,CHAN=B")
-dbLoadRecords("$(TOP)/db/lakeshore336_chan.db","Sys=XF:23ID1-ES,Dev={TCtrl:1-Chan:C},PORT=LAKE2,CHAN=C")
-dbLoadRecords("$(TOP)/db/lakeshore336_chan.db","Sys=XF:23ID1-ES,Dev={TCtrl:1-Chan:D},PORT=LAKE2,CHAN=D")
-dbLoadRecords("$(TOP)/db/lakeshore336_loop.db","Sys=XF:23ID1-ES,Dev={TCtrl:1-Out:1},PORT=LAKE2,CHAN=1")
-dbLoadRecords("$(TOP)/db/lakeshore336_loop.db","Sys=XF:23ID1-ES,Dev={TCtrl:1-Out:2},PORT=LAKE2,CHAN=2")
+dbLoadRecords("$(TOP)/db/lakeshore336_chan.db","Sys=XF:23ID1-ES,Dev={TCtrl:1-Chan:A},PORT=LAKE1,CHAN=A")
+dbLoadRecords("$(TOP)/db/lakeshore336_chan.db","Sys=XF:23ID1-ES,Dev={TCtrl:1-Chan:B},PORT=LAKE1,CHAN=B")
+dbLoadRecords("$(TOP)/db/lakeshore336_chan.db","Sys=XF:23ID1-ES,Dev={TCtrl:1-Chan:C},PORT=LAKE1,CHAN=C")
+dbLoadRecords("$(TOP)/db/lakeshore336_chan.db","Sys=XF:23ID1-ES,Dev={TCtrl:1-Chan:D},PORT=LAKE1,CHAN=D")
+dbLoadRecords("$(TOP)/db/lakeshore336_loop.db","Sys=XF:23ID1-ES,Dev={TCtrl:1-Out:1},PORT=LAKE1,CHAN=1")
+dbLoadRecords("$(TOP)/db/lakeshore336_loop.db","Sys=XF:23ID1-ES,Dev={TCtrl:1-Out:2},PORT=LAKE1,CHAN=2")
+
+dbLoadRecords("$(TOP)/db/lakeshore336_chan.db","Sys=XF:23ID1-ES,Dev={TCtrl:2-Chan:A},PORT=LAKE2,CHAN=A")
+dbLoadRecords("$(TOP)/db/lakeshore336_chan.db","Sys=XF:23ID1-ES,Dev={TCtrl:2-Chan:B},PORT=LAKE2,CHAN=B")
+dbLoadRecords("$(TOP)/db/lakeshore336_chan.db","Sys=XF:23ID1-ES,Dev={TCtrl:2-Chan:C},PORT=LAKE2,CHAN=C")
+dbLoadRecords("$(TOP)/db/lakeshore336_chan.db","Sys=XF:23ID1-ES,Dev={TCtrl:2-Chan:D},PORT=LAKE2,CHAN=D")
+dbLoadRecords("$(TOP)/db/lakeshore336_loop.db","Sys=XF:23ID1-ES,Dev={TCtrl:2-Out:1},PORT=LAKE2,CHAN=1")
+dbLoadRecords("$(TOP)/db/lakeshore336_loop.db","Sys=XF:23ID1-ES,Dev={TCtrl:2-Out:2},PORT=LAKE2,CHAN=2")
 
 asSetFilename("/epics/xf/23id/xf23id.acf")
 
